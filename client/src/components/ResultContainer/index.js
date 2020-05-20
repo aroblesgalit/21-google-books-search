4e10f7fb4e1dccfa2fe1d4b53df6e0fb76b9e7e1
Alvin Galit <aroblesgalit@gmail.com>
2020-05-20 14:02:56 -0500

    Created api

--- client/src/components/ResultContainer/index.js
+++ client/src/components/ResultContainer/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./style.css";
+import ResultCard from "../ResultCard";
+import API from "../../utils/API";
 
 function ResultContainer() {
+
+
+    API.getBooksByQuery("Harry Potter")
+        .then(res =>
+            console.log(res)
+        )
+        .catch(err => console.log(err));
+
     return (
         <div className="resultContainer">
             <h3>Results</h3>
+            <ResultCard />
         </div>
     );
 }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -16,5 +16,27 @@ export default {
   // Saves a book to the database
   saveBook: function(bookData) {
     return axios.post("/api/books", bookData);
+  },
+
+  getBooksByQuery: function(query) {
+    let queryUrl = "https://www.googleapis.com/books/v1/volumes?q=" + query + "&key=AIzaSyAFGnOOlvmSYxkEqvTpfuWI9nS1PTfzk-U";
+    return new Promise((resolve, reject) => {
+      axios
+        .get(queryUrl)
+        .then(res => {
+          const books = res.data.items;
+          const results = books.map(book => {
+            return {
+              title: book.volumeInfo.title,
+              authors: book.volumeInfo.authors,
+              description: book.volumeInfo.description,
+              image: book.volumeInfo.imageLinks.thumbnail,
+              link: book.volumeInfo.infoLink
+            };
+          });
+          resolve(results);
+        })
+        .catch(err => reject(err));
+    });
   }
 };
